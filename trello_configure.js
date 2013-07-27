adf5cd4e39be320d1851a7a278fb4a10e2bd1d98
Robert Lowe <robert@iblargz.com>
2013-07-27 01:54:23 -0400

    Add additional properties

--- trello_configure.js
+++ trello_configure.js
@@ -7,5 +7,8 @@ Template.configureLoginServiceDialogForTrello.fields = function () {
   return [
     {property: 'consumerKey', label: 'Consumer key'},
     {property: 'secret', label: 'Consumer secret'}
+    {property: 'name', label: 'Application Name'}
+    {property: 'scope', label: 'Scope (or leave blank)'}
+    {property: 'expiration', label: 'Expiration (or leave blank)'}
   ];
 };
